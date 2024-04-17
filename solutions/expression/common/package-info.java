1f870e771ba8003d3b2afed9c59023af2b78fcba
GeorgeGolenkov1 <149122142+GeorgeGolenkov1@users.noreply.github.com>
2024-04-17 16:22:56 +0300

    Delete solutions/expression/common directory

--- solutions/expression/common/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Expressions generators for expression-based homeworks
- * of <a href="https://www.kgeorgiy.info/courses/prog-intro/">Introduction to Programming</a> course.
- *
- * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
- */
-package expression.common;
