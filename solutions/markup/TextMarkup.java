b1a3c9817b9a28d164b8413939b547b4bf1308ec
ITStrongBeast <149122142+ITStrongBeast@users.noreply.github.com>
2024-08-11 17:37:11 +0500

    Update TextMarkup.java

--- solutions/markup/TextMarkup.java
+++ solutions/markup/TextMarkup.java
@@ -1,16 +1,19 @@
 package markup;
 
 import java.util.List;
+
 public abstract class TextMarkup extends Elements {
+    
     private  final List<Elements> list;
     private  final String teg;
     private  final String teg1;
 
-    public TextMarkup (List<Elements> list, String tegs, String tegs1){
+    public TextMarkup (List<Elements> list, String tegs, String tegs1) {
         this.list = list;
         this.teg = tegs;
         this.teg1 = tegs1;
     }
+    
     @Override
     public void toMarkdown(StringBuilder str) {
         str.append(teg);
@@ -19,6 +22,7 @@ public abstract class TextMarkup extends Elements {
         }
         str.append(teg);
     }
+    
     @Override
     public void toBBCode(StringBuilder str) {
         str.append("[");
