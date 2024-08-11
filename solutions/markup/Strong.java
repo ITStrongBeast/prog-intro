9853e968d83bbd2d1a2ed8321b836cff2b5b7585
ITStrongBeast <149122142+ITStrongBeast@users.noreply.github.com>
2024-08-11 17:36:10 +0500

    Update Strong.java

--- solutions/markup/Strong.java
+++ solutions/markup/Strong.java
@@ -1,8 +1,10 @@
 package markup;
 
 import java.util.List;
-public class Strong extends TextMarkup{
-    public Strong (List<Elements> list){
+
+public class Strong extends TextMarkup {
+    
+    public Strong (List<Elements> list) {
         super(list, "__", "b");
     }
 }
