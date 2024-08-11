52743f693d1621f27787619264351c4cd9a10ff3
ITStrongBeast <149122142+ITStrongBeast@users.noreply.github.com>
2024-08-11 17:36:34 +0500

    Update Text.java

--- solutions/markup/Text.java
+++ solutions/markup/Text.java
@@ -1,11 +1,15 @@
 package markup;
 
 import java.util.List;
+
 public class Text extends Elements{
+    
     String string;
+    
     public Text(String st) {
         this.string = st;
     }
+    
     @Override
     public void toMarkdown(StringBuilder str) {
         str.append(this.string);
